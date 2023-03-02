7a129a1f5b8140040033662ca94d4d91525f838a
danko beribak <beribakdanko@gmail.com>
2023-03-02 10:24:25 +0100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'movies/index'
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :movies, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
